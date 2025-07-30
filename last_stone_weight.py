3b20c5a35218e02ded7c2fc733c5d3cdf34ca543
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-07-30 08:58:59 +0530

    last stone weight 1046

---
+++ last_stone_weight.py
@@ -0,0 +1,31 @@
+# 1046. Last Stone Weight
+# Example 1:
+# Input: stones = [2,7,4,1,8,1]
+# Output: 1
+# Explanation:
+# We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+# we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+# we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+# we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+#
+# Example 2:
+# Input: stones = [1]
+# Output: 1
+from heapq import heapify, heappush, heappop
+from typing import List
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        max_heap = [-stone for stone in stones]      # invert the values
+        heapify(max_heap)
+
+        while len(max_heap) > 1:
+            stone1 = -heappop(max_heap)
+            stone2 = -heappop(max_heap)
+            if stone1 != stone2:
+                heappush(max_heap, -(stone1 - stone2))
+
+        return 0 if not max_heap else -max_heap[0]
+
+s=Solution()
+print(s.lastStoneWeight([2,7,4,1,8,1]))
+print(s.lastStoneWeight([1]))
