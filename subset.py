e6022b6e60fe6b9e561626d531512ed9e7ecdbf8
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-08-09 18:25:56 +0530

    subsets 78

---
+++ subset.py
@@ -0,0 +1,32 @@
+# 78  Subsets
+# Example 1:
+# Input: nums = [1,2,3]
+# Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+#
+# Example 2:
+# Input: nums = [0]
+# Output: [[],[0]]
+from typing import List
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+
+        def backtrack(index, path):
+
+            if index==len(nums):
+                result.append(path[:])
+                return
+
+            path.append(nums[index])
+            backtrack(index+1,path)
+            path.pop()
+
+
+            backtrack(index+1,path)
+
+        backtrack(0,[])
+        return result
+
+s=Solution()
+print(s.subsets([1,2,3]))
+print(s.subsets([0]))
