07aaa983bb54bb2a104271f926fb0f45a46d7b8a
Pugalendi A <39829202+PugalendiArumugam@users.noreply.github.com>
2025-08-25 12:09:58 +0530

    word later

---
+++ word_ladder.py
@@ -0,0 +1,14 @@
+# Example 1:
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+# Output: 5
+# Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+#
+# Example 2:
+# Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+# Output: 0
+# Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+
+from typing import List
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        pass
